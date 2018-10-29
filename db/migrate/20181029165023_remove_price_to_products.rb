3bbc98fb27d515312eceec2dda59d1ff6b82071c
Kaue Matteucci <kauefm@gmail.com>
2018-10-29 18:20:11 -0200

    Migration revised

--- db/migrate/20181029165023_remove_price_to_products.rb
+++
@@ -1,6 +0,0 @@
-class RemovePriceToProducts < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :products, :price, :float
-    remove_column :products, :price_cents, :integer
-  end
-end
