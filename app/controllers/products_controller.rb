c6f668c302ce81f065919c0114a3d0c07f4a215d
Kaue Matteucci <kauefm@gmail.com>
2018-10-29 11:21:47 -0200

    Routed created and production action and view done

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,57 @@
 class ProductsController < ApplicationController
-  def edit
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
+  helper_method :number_to_currency_br
+
+  def new
+      @product = Product.new
   end
 
   def index
+    @products = Product.all
+    @clients = Client.all
   end
 
-  def new
+  def show
   end
+
+  def edit
+  end
+
+  def destroy
+    @product.destroy
+    ProductMailer.erased(@product).deliver_now
+    redirect_to root_path
+  end
+
+  def create
+    @product = Product.new(product_params)
+      if Client.nil?
+        client = Client.new(name:"Kauê" , email:"kauefm@gmail.com")
+      else
+        client = Client.last
+      end
+    @product.client = client
+    if @product.save
+        ProductMailer.created(@product).deliver_now
+       redirect_to products_path
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @product.update(product_params)
+    ProductMailer.edited(@product).deliver_now
+    redirect_to products_path
+  end
+
+  private
+
+    def product_params
+      params.require(:product).permit(:description,:price,:quantity)
+    end
+
+    def set_product
+      @product = Product.find(params[:id])
+    end
 end
