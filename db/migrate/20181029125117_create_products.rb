fa4382a8fe017f64724f3e4abf784031fa2ce4b6
Kaue Matteucci <kauefm@gmail.com>
2018-10-29 18:33:08 -0200

    Heroku corretion

--- db/migrate/20181029125117_create_products.rb
+++ db/migrate/20181029125117_create_products.rb
@@ -2,7 +2,7 @@ class CreateProducts < ActiveRecord::Migration[5.1]
   def change
     create_table :products do |t|
       t.string :description
-      t.string :price
+      t.float :price
       t.integer :quantity
 
       t.timestamps
